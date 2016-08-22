3ff93b2439f6a5f154484e0d32032f724b23a3a5
YYH <yangyonghao@yhxbank.com>
2016-08-22 23:20:54 +0800

    后台admin

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -6,7 +6,7 @@ class IndexController extends Controller {
     public function index(){
         if(empty($_SESSION['u_msg'])){
             //param：url 参数 跳转时间 提示信息
-            $this->redirect('Login/login','',1,'请先登录!');
+            $this->redirect('Login/login');
         }else{
 
             $this->display();
